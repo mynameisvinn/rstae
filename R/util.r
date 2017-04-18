a9d558e20725a084763781b369d0ee53fd651a80
mynameisvinn <vin.tang@gmail.com>
2017-04-17 20:44:36 -0400

    namespace management with external libraries

--- R/util.r
+++ R/util.r
@@ -8,7 +8,7 @@ test <- function(woof){
 #' @export
 fetch_trips <-function(municipalId){
   uri = paste("https://municipal.systems/v1/municipalities/", municipalId, "/trips", sep="")
-  r <- GET(uri)
+  r <- httr::GET(uri)
   e = content(r)$results
   f = as.data.frame(do.call(rbind, e))
   f
